38a238fa7d1bf1b927d91597fd8dbf18d8237ce0
Grigory <grg@grgnote.(none)>
2011-10-26 23:02:30 +0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Demogmaps4rails::Application.config.secret_token = '999b291a3a58686a488602e0e4bb58a8c8b2fc7a6114cff619d8001817daf4b594f4d3a01e004750a732a41193cd20137b20114557f7c9d93c590e90551df99e'
